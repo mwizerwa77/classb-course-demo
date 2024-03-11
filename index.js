2f7554bce0ddbc0672fb43befe0e2ef89831e922
mwizerwa77 <mwizerwa77@gmail.com>
2024-03-11 14:51:40 +0200

    initial commit containing a fix bug in add function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 module.exports.add=function add(num1,num2){
     return num1+num2
 }
+
+module.exports.sub=function sub(num1,num2){
+    return num1+num2
+}
+
+module.exports.multply=function multply(num1,num2){
+    return num1+num2
+}
